a0c3d10b5e1be28a8a69979a288e5817c4053d7b
anhdungadg <cscssl@gmail.com>
2024-09-22 22:51:02 +0700

    Follow the HIPAA guide, add S3 logging

--- s3_bucket/main.tf
+++ s3_bucket/main.tf
@@ -8,13 +8,29 @@ variable "environment" {
 }
 
 resource "aws_s3_bucket" "this" {
+  depends_on = [ aws_s3_bucket.log_bucket ]
   bucket = var.bucket_name
+
+  logging {
+    target_bucket = aws_s3_bucket.log_bucket.id
+    target_prefix = "log/"
+  }
   
   tags = {
     Name = var.bucket_name
     Environment = var.environment
   }
 }
+
+resource "aws_s3_bucket" "log_bucket" {
+  bucket = "${var.bucket_name}-logs"
+
+  tags = {
+    Name = "${var.bucket_name}-logs"
+    Environment = var.environment
+  }
+}
+
 
 output "bucket_name" {
   value = aws_s3_bucket.this.bucket
